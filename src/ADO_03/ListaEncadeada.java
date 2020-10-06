1b13f220ea225e94a546fb15e58098391a2ad35e
Ivan <Ivan@LAPTOP-GFV74OKO>
2020-10-06 17:35:23 -0300

    Alteração LAB 2

--- src/ADO_03/ListaEncadeada.java
+++ src/ADO_03/ListaEncadeada.java
@@ -10,34 +10,92 @@ package ADO_03;
  * @author ivanyuratakano
  */
 public class ListaEncadeada {
+
     private No ini;
-    
+
     // Criar um lista vazia
-    public ListaEncadeada(){
+    public ListaEncadeada() {
         this.ini = null;
     }
-    
+
     // Verifica se a lista está vazia
-    public boolean vazia(){
+    public boolean vazia() {
         return this.ini == null;
     }
-    
+
     public void insereInicio(int elemento) {
         No novo = new No(elemento, ini);
         ini = novo;
     }
-    
+
     @Override
     public String toString() {
         String strLista = "";
         No temp = ini;
-        
-        while( temp != null){
+
+        while (temp != null) {
             strLista = strLista + temp.getElemento() + " ";
             temp = temp.getProx();
         }
         return strLista;
     }
-    
-    
+
+    public void unirListas(ListaEncadeada lista, ListaEncadeada lista2) {
+       
+        if (lista != null && lista2 != null) {
+
+            No temp = lista.ini;
+
+            while (temp.getProx() != null) {
+                temp = temp.getProx();
+            }
+            temp.setProx(lista2.ini);
+            lista2.ini = null;
+        }
+    }
+
+    public void intercalar(ListaEncadeada lista, ListaEncadeada lista2, ListaEncadeada lista3) {
+       
+        if (lista != null && lista2 != null) {
+
+            No temp = lista.ini;
+            No temp2 = lista2.ini;
+
+            while (temp != null || temp2 != null) {
+                lista3.insereInicio(temp.getElemento());
+                lista3.insereInicio(temp2.getElemento());
+                temp = temp.getProx();
+                temp2 = temp2.getProx();
+            }
+
+            while (temp != null) {
+                lista3.insereInicio(temp.getElemento());
+                temp.getProx();
+            }
+
+            while (temp2 != null) {
+                lista3.insereInicio(temp2.getElemento());
+                temp2.getProx();
+            }
+        }
+    }
+
+    public void intersecção(ListaEncadeada lista, ListaEncadeada lista2, ListaEncadeada lista4) {
+        
+        if (lista != null && lista2 != null) {
+
+            No temp = lista.ini;
+            
+            while (temp != null) {
+                No temp2 = lista2.ini;
+                while (temp2 != null) {
+                    if (temp.getElemento() == temp2.getElemento()) {
+                        lista4.insereInicio(temp.getElemento());                       
+                    }
+                        temp2 = temp2.getProx();                               
+                }
+                temp = temp.getProx();                
+            }
+        }
+    }
 }
--- src/ADO_03/Teste.java
+++ src/ADO_03/Teste.java
@@ -13,6 +13,8 @@ public class Teste {
     public static void main(String[] args) {
         ListaEncadeada lista = new ListaEncadeada();
         ListaEncadeada lista2 = new ListaEncadeada();
+        ListaEncadeada lista3 = new ListaEncadeada();
+        ListaEncadeada lista4 = new ListaEncadeada();
         System.out.println(lista);
         System.out.println(lista.vazia());
         lista.insereInicio(7);
@@ -21,9 +23,15 @@ public class Teste {
         lista.insereInicio(1);
         System.out.println(lista);
         lista2.insereInicio(8);
-        lista2.insereInicio(6);
-        lista2.insereInicio(4);
+        lista2.insereInicio(5);
+        lista2.insereInicio(3);
         lista2.insereInicio(2);
-        System.out.println(lista2);
+        System.out.println(lista2);            
+        lista3.intercalar(lista, lista2, lista3);
+        System.out.println("Intercalação: " + lista3);
+        lista4.intersecção(lista, lista2, lista4);
+        System.out.println("Intersecção: " + lista4);
+        lista.unirListas(lista, lista2);
+        System.out.println("União: " + lista);  
     }
 }
